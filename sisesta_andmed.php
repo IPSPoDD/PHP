f13bd65de12bfdcce97679c779b61ebb979679ef
IPSPoDD <57214472+IPSPoDD@users.noreply.github.com>
2022-04-28 17:44:46 +0300

    Create sisesta_andmed.php

---
+++ sisesta_andmed.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="UTF-8">
+	<head>
+	<title>Andmete sisestamine 1</title>
+	</head>
+	<body>
+	
+	<form action="includes/signup.php" method="POST">
+	<br>
+	<input type="text" name="kasutaja_eesn" placeholder="Eesnimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_peren" placeholder="Perekonnanimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_email" placeholder="E-mail">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_kasutajan" placeholder="Kasutajanimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_pwd" placeholder="Parool">
+	<br>
+	<br>
+	<button type="submit" name="Submit"> Registreeri</button>
+	</form>
+	
+	</body>
+</html>
