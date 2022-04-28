950ca4da22bb392375ac88d3f8a8cbbc15c8acda
IPSPoDD <57214472+IPSPoDD@users.noreply.github.com>
2022-04-28 20:58:30 +0300

    Create sisestaandmed3.php

---
+++ turvalisus/sisestaandmed3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="UTF-8">
+	<head>
+	<title>Andmete sisestamine 3</title>
+	</head>
+	<body>
+	
+	<form action="includes/signup3.php" method="POST"> <!-- Action toimub lehel 2, sinna viiakse info kasuta POST -->
+	<br>
+	<input type="text" name="kasutaja_eesn" placeholder="Eesnimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_peren" placeholder="Perekonnanimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_email" placeholder="E-mail">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_kasutnim" placeholder="Kasutajanimi">
+	<br>
+	<br>
+	<input type="text" name="kasutaja_pwd" placeholder="Parool">
+	<br>
+	<br>
+	<button type="submit" name="Submit"> Registreeri</button>
+	</form>
+	
+	</body>
+</html>
